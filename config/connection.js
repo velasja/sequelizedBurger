1c156a72a934b7bc65b3ba6f62f1de471b7407a6
JJ Velasquez <velas.ja@gmail.com>
2017-02-15 00:36:54 -0600

    once more

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,13 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
-		host: "us-cdbr-iron-east-04.cleardb.net",
-		port: 8889,
-		user: "b4bcde2e179a2a",
-		password: "03538694",
-		database: "heroku_d17878de573443a"
-	});
-};
+connection = mysql.createConnection({
+	host: "us-cdbr-iron-east-04.cleardb.net",
+	port: 8889,
+	user: "b4bcde2e179a2a",
+	password: "03538694",
+	database: "heroku_d17878de573443a"
+});
 
 connection.connect();
 
