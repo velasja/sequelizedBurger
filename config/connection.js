8cdb277f049315ec134c95dd87ae49fb36ab232b
JJ Velasquez <velas.ja@gmail.com>
2017-02-15 01:01:17 -0600

    testing

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 var mysql = require("mysql");
 var connection;
+var port = process.env.PORT | 3000;
 
 connection = mysql.createConnection({
 	host: "us-cdbr-iron-east-04.cleardb.net",
-	port: 8889,
 	user: "b4bcde2e179a2a",
 	password: "03538694",
 	database: "heroku_d17878de573443a"
 });
 
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
