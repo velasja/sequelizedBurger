c3525cc1e70b329d41b880886224ffb67b0ac82f
JJ Velasquez <velas.ja@gmail.com>
2017-02-14 23:16:43 -0600

    more debugging

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+	console.log(this, "I'm working");
 } else {
 	connection = mysql.createConnection({
 		host: "localhost",
