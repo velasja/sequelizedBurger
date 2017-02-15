d97cdb91299a6ec60755ff8514d98f7d14f203e8
JJ Velasquez <velas.ja@gmail.com>
2017-02-15 00:49:22 -0600

    cleardb file

---
+++ my_dump_file.sql
@@ -0,0 +1,55 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.34)
+# Database: burgers_db
+# Generation Time: 2017-02-15 06:44:53 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table burgers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `burgers`;
+
+CREATE TABLE `burgers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `burger_name` varchar(255) NOT NULL,
+  `devoured` tinyint(1) DEFAULT '0',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `burgers` WRITE;
+/*!40000 ALTER TABLE `burgers` DISABLE KEYS */;
+
+INSERT INTO `burgers` (`id`, `burger_name`, `devoured`, `created_on`)
+VALUES
+	(1,'Big Mac',1,'2017-02-12 11:18:02'),
+	(2,'Goodburger',0,'2017-02-12 11:18:02'),
+	(3,'Krabby Patty',0,'2017-02-12 11:18:02');
+
+/*!40000 ALTER TABLE `burgers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
