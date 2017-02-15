59a1441f35b542c617ed69d730361dab7eaf2200
JJ Velasquez <velas.ja@gmail.com>
2017-02-15 00:29:53 -0600

    clearDB

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,13 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-	console.log(this, "I'm working");
 } else {
 	connection = mysql.createConnection({
-		host: "localhost",
+		host: "us-cdbr-iron-east-04.cleardb.net",
 		port: 8889,
-		user: "root",
-		password: "root",
-		database: "burgers_db"
+		user: "b4bcde2e179a2a",
+		password: "03538694",
+		database: "heroku_d17878de573443a"
 	});
 };
 
