8b1623579402add68c3e2f6ce960870f86e96841
JJ Velasquez <velas.ja@gmail.com>
2017-02-15 18:24:14 -0600

    pleaaase

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 var connection;
 
-connection = mysql.createConnection({
-	host: "us-cdbr-iron-east-04.cleardb.net",
-	user: "b4bcde2e179a2a",
-	password: "03538694",
-	database: "heroku_d17878de573443a"
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "root",
+		database: "burgers_db"
+	});
+};
 
 connection.connect(function(err) {
   if (err) {
