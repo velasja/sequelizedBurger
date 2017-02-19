39dc5e997eb03c1ba03cfc84a6495ee392ae0f20
JJ Velasquez <velas.ja@gmail.com>
2017-02-19 14:51:53 -0600

    second commit

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,45 +1,37 @@
 var express = require("express");
-var burger = require("../models/burger.js");
+var db = require("../models");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-	burger.selectAll(function(data) {
-		var hbsObject = {
-		  burgers: data
-		};
-		console.log(hbsObject);
-		res.render("index", hbsObject);
-	});
+	db.Burger.findAll({})
+  .then(function(dbBurger) {
+    res.json(dbBurger);
+  })
+		
 });
 
 router.post("/", function(req, res) {
-  burger.insertOne([
-    "id", "burger_name"
-  ], [
-    req.body.id, req.body.burger_name
-  ], function() {
-    res.redirect("/");
+  db.Burger.create({
+    burger_name: req.body.burger_name,
+    devoured: req.body.devoured,
+    created_on: req.body.created_on
+  }).then(function(dbBurger) {
+    res.json(dbBurger);
   });
+   
 });
 
 router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
-  });
+  db.Burger.update(req.body,
+    {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbBurger) {
+      res.json(dbBurger);
+    });
+ 
 });
-
-// router.delete("/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-//   burger.delete(condition, function() {
-//     res.redirect("/");
-//   });
-// });
 
 // Export routes for server.js to use.
 module.exports = router;
